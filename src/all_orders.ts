7a95d1ca085ad1602f993dd17099c9a648e3fe1c
fabioberger <me@fabioberger.com>
2020-01-03 14:16:32 +0100

    Rename variable for clarity

--- src/all_orders.ts
+++ src/all_orders.ts
@@ -3,9 +3,9 @@ import { WSClient } from '@0x/mesh-rpc-client';
 (async () => {
     const c = new WSClient("ws://localhost:60557");
 
-    const orders = await c.getOrdersAsync();
+    const getOrdersResponse = await c.getOrdersAsync();
 
-    console.log(`Got ${orders.ordersInfos.length} orders!`);
+    console.log(`Got ${getOrdersResponse.ordersInfos.length} orders!`);
 
     process.exit(0);
 })()
